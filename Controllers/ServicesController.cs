cb2b1c26d8411e907f1ceb3b8780e0ca86f81a56
Ben <FudgeApps@users.noreply.github.com>
2014-06-10 22:37:32 +0100

    Added ServiceController.cs

--- Controllers/ServicesController.cs
+++ Controllers/ServicesController.cs
@@ -13,7 +13,7 @@ using OpenGovApi.Models;
 
 namespace OpenGovApi.Controllers
 {
-    [RoutePrefix("Service")]
+    [RoutePrefix("Services")]
     public class ServicesController : ApiController
     {
         private OpenGovContext db = new OpenGovContext();
@@ -22,71 +22,6 @@ namespace OpenGovApi.Controllers
         public IQueryable<Service> GetServices()
         {
             return db.Services;
-        }
-
-        [HttpGet]
-        [Route("{id}")]
-        [ResponseType(typeof(Service))]
-        public async Task<IHttpActionResult> GetService(int id)
-        {
-            Service service = await db.Services.FindAsync(id);
-            if (service == null)
-            {
-                return NotFound();
-            }
-
-            return Ok(service);
-        }
-
-        [HttpPost]
-        [Route("{id}")]
-        [ResponseType(typeof(void))]
-        public async Task<IHttpActionResult> PutService(int id, Service service)
-        {
-            if (!ModelState.IsValid)
-            {
-                return BadRequest(ModelState);
-            }
-
-            if (id != service.Id)
-            {
-                return BadRequest();
-            }
-
-            db.Entry(service).State = System.Data.Entity.EntityState.Modified;
-
-            try
-            {
-                await db.SaveChangesAsync();
-            }
-            catch (DbUpdateConcurrencyException)
-            {
-                if (!ServiceExists(id))
-                {
-                    return NotFound();
-                }
-                else
-                {
-                    throw;
-                }
-            }
-
-            return StatusCode(HttpStatusCode.NoContent);
-        }
-        
-        [HttpPost]
-        [ResponseType(typeof(Service))]
-        public async Task<IHttpActionResult> PostService(Service service)
-        {
-            if (!ModelState.IsValid)
-            {
-                return BadRequest(ModelState);
-            }
-
-            db.Services.Add(service);
-            await db.SaveChangesAsync();
-
-            return CreatedAtRoute("DefaultApi", new { id = service.Id }, service);
         }
 
         protected override void Dispose(bool disposing)
